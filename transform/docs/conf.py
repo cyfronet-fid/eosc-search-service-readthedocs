1f9c83cb8ed56afaabe947d15cf9e9ccdbef7ca8
wiktorflorian <wiktorflorianwf@gmail.com>
2024-09-06 12:07:19 +0200

    readthedocs test

--- transform/docs/conf.py
+++ transform/docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.todo",
     "sphinx_autodoc_typehints",
-    "autodoc_pydantic",
+    "sphinxcontrib.autodoc_pydantic",
 ]
 
 templates_path = ["_templates"]
